7f901b94067d413ea03ca3ae69eb80e2ad4e78d3
Fabio Di Fabio <fabio.difabio@consensys.net>
2021-10-20 14:40:42 +0200

    Assert directly Path objects instead of converting them to string
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/query/StateBackupServiceTest.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/query/StateBackupServiceTest.java
@@ -28,41 +28,41 @@ public class StateBackupServiceTest {
 
   @Test
   public void fileIndexRenames() {
-    assertThat(StateBackupService.dataFileToIndex(Path.of("/tmp/besu-blocks-0000.cdat")).toString())
-        .isEqualTo("/tmp/besu-blocks.cidx");
-    assertThat(StateBackupService.dataFileToIndex(Path.of("/tmp/besu.blocks.0000.rdat")).toString())
-        .isEqualTo("/tmp/besu.blocks.ridx");
+    assertThat(StateBackupService.dataFileToIndex(Path.of("/tmp/besu-blocks-0000.cdat")))
+        .isEqualTo(Path.of("/tmp/besu-blocks.cidx"));
+    assertThat(StateBackupService.dataFileToIndex(Path.of("/tmp/besu.blocks.0000.rdat")))
+        .isEqualTo(Path.of("/tmp/besu.blocks.ridx"));
   }
 
   @Test
   public void leafFileName() {
-    assertThat(StateBackupService.accountFileName(backupDir, 4_000_000, 42, false).toString())
-        .isEqualTo("/tmp/backup/besu-account-backup-04000000-0042.rdat");
-    assertThat(StateBackupService.accountFileName(backupDir, 6_000_000, 46, true).toString())
-        .isEqualTo("/tmp/backup/besu-account-backup-06000000-0046.cdat");
+    assertThat(StateBackupService.accountFileName(backupDir, 4_000_000, 42, false))
+        .isEqualTo(Path.of("/tmp/backup/besu-account-backup-04000000-0042.rdat"));
+    assertThat(StateBackupService.accountFileName(backupDir, 6_000_000, 46, true))
+        .isEqualTo(Path.of("/tmp/backup/besu-account-backup-06000000-0046.cdat"));
   }
 
   @Test
   public void headerFileName() {
-    assertThat(StateBackupService.headerFileName(backupDir, 42, false).toString())
-        .isEqualTo("/tmp/backup/besu-header-backup-0042.rdat");
-    assertThat(StateBackupService.headerFileName(backupDir, 46, true).toString())
-        .isEqualTo("/tmp/backup/besu-header-backup-0046.cdat");
+    assertThat(StateBackupService.headerFileName(backupDir, 42, false))
+        .isEqualTo(Path.of("/tmp/backup/besu-header-backup-0042.rdat"));
+    assertThat(StateBackupService.headerFileName(backupDir, 46, true))
+        .isEqualTo(Path.of("/tmp/backup/besu-header-backup-0046.cdat"));
   }
 
   @Test
   public void bodyFileName() {
-    assertThat(StateBackupService.bodyFileName(backupDir, 42, false).toString())
-        .isEqualTo("/tmp/backup/besu-body-backup-0042.rdat");
-    assertThat(StateBackupService.bodyFileName(backupDir, 46, true).toString())
-        .isEqualTo("/tmp/backup/besu-body-backup-0046.cdat");
+    assertThat(StateBackupService.bodyFileName(backupDir, 42, false))
+        .isEqualTo(Path.of("/tmp/backup/besu-body-backup-0042.rdat"));
+    assertThat(StateBackupService.bodyFileName(backupDir, 46, true))
+        .isEqualTo(Path.of("/tmp/backup/besu-body-backup-0046.cdat"));
   }
 
   @Test
   public void receiptFileName() {
-    assertThat(StateBackupService.receiptFileName(backupDir, 42, false).toString())
-        .isEqualTo("/tmp/backup/besu-receipt-backup-0042.rdat");
-    assertThat(StateBackupService.receiptFileName(backupDir, 46, true).toString())
-        .isEqualTo("/tmp/backup/besu-receipt-backup-0046.cdat");
+    assertThat(StateBackupService.receiptFileName(backupDir, 42, false))
+        .isEqualTo(Path.of("/tmp/backup/besu-receipt-backup-0042.rdat"));
+    assertThat(StateBackupService.receiptFileName(backupDir, 46, true))
+        .isEqualTo(Path.of("/tmp/backup/besu-receipt-backup-0046.cdat"));
   }
 }
