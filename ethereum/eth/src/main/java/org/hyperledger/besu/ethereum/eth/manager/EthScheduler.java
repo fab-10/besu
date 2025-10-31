150aad63ea3e7daba404e20a0e0ffd50d3bc64c4
Fabio Di Fabio <fabio.difabio@consensys.net>
2025-10-31 20:59:52 +0100

    Move tx precomputations to Computation executor
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/EthScheduler.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/EthScheduler.java
@@ -76,7 +76,7 @@ public class EthScheduler {
       final MetricsSystem metricsSystem) {
     this(
         MonitoredExecutors.newFixedThreadPool(
-            EthScheduler.class.getSimpleName() + "-Workers", syncWorkerCount, metricsSystem),
+            EthScheduler.class.getSimpleName() + "-SyncWorkers", syncWorkerCount, metricsSystem),
         MonitoredExecutors.newScheduledThreadPool(
             EthScheduler.class.getSimpleName() + "-Timer", 1, metricsSystem),
         MonitoredExecutors.newBoundedThreadPool(
