5efa91599e3aad4bd13706dbd0551f56e1b0f186
Fabio Di Fabio <fabio.difabio@consensys.net>
2026-05-01 10:57:26 +0200

    Fix rebase
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/transactions/inclusionlist/InclusionListValidator.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/transactions/inclusionlist/InclusionListValidator.java
@@ -20,7 +20,6 @@ import org.hyperledger.besu.ethereum.BlockProcessingOutputs;
 import org.hyperledger.besu.ethereum.BlockProcessingResult;
 import org.hyperledger.besu.ethereum.ProtocolContext;
 import org.hyperledger.besu.ethereum.core.BlockHeader;
-import org.hyperledger.besu.ethereum.core.MutableWorldState;
 import org.hyperledger.besu.ethereum.core.Transaction;
 import org.hyperledger.besu.ethereum.core.encoding.EncodingContext;
 import org.hyperledger.besu.ethereum.core.encoding.TransactionDecoder;
@@ -34,6 +33,7 @@ import org.hyperledger.besu.ethereum.transaction.TransactionInvalidReason;
 import org.hyperledger.besu.ethereum.trie.pathbased.common.provider.WorldStateQueryParams;
 import org.hyperledger.besu.evm.blockhash.BlockHashLookup;
 import org.hyperledger.besu.evm.tracing.OperationTracer;
+import org.hyperledger.besu.plugin.services.worldstate.MutableWorldState;
 
 import java.util.List;
 import java.util.Optional;
