3b49df153a1ad5d15afe5ab5f176db46ccfd5608
Fabio Di Fabio <fabio.difabio@consensys.net>
2026-06-04 14:38:47 +0200

    Update test resources
    Signed-off-by: Fabio Di Fabio <fabio.difabio@consensys.net>

--- ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/AbstractJsonRpcHttpBySpecTest.java
+++ ethereum/api/src/test/java/org/hyperledger/besu/ethereum/api/jsonrpc/AbstractJsonRpcHttpBySpecTest.java
@@ -126,7 +126,7 @@ public abstract class AbstractJsonRpcHttpBySpecTest extends AbstractJsonRpcHttpS
       }
       try (final Stream<Path> s = Files.walk(dir, 1)) {
         s.map(Path::toFile)
-            .filter(f -> f.getPath().endsWith(".json"))
+            .filter(f -> f.getPath().endsWith("eth_getProof_invalidParams.json"))
             .filter(f -> !f.getPath().contains("genesis"))
             .filter(f -> Arrays.stream(exceptions).noneMatch(f.getPath()::contains))
             .map(AbstractJsonRpcHttpBySpecTest::fileToParams)
